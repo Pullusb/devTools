445b396ebe7b0d60271ff6a0b9fce520e703bba1
Samuel Bernou <bernou.samuel@gmail.com>
2017-03-21 12:33:36 +0100

    Add files via upload

---
+++ SB_devTools.py
@@ -0,0 +1,186 @@
+bl_info = {
+    "name": "dev tools",
+    "description": "Add tool to help developpement",
+    "author": "Samuel Bernou",
+    "version": (0, 0, 1),
+    "blender": (2, 78, 0),
+    "location": "Text editor > toolbar",
+    "warning": "",
+    "wiki_url": "",
+    "category": "Text Editor" }
+
+
+import bpy
+import os
+import re
+
+###---UTILITY funcs
+
+def copySelected():
+    '''Copy selected Text'''
+
+    bpy.ops.text.copy()
+    clip = bpy.context.window_manager.clipboard
+    return (clip)
+
+
+def print_string_variable(clip,linum=''):
+    if linum:
+        line = 'print("l{1}:{0}", {0})#Dbg'.format(clip, str(linum) )
+    else:
+        line = 'print("{0}", {0})#Dbg'.format(clip)
+    #'print("'+ clip + '", ' + clip + ')#Dbg'
+    return (line)
+
+
+def Fixindentation(Loaded_text, charPos):
+    '''take text and return it indented according to cursor position'''
+
+    FormattedText = Loaded_text
+    # print("FormattedText", FormattedText)#Dbg
+    if charPos > 0:
+        textLines = Loaded_text.split('\n')
+        if not len(textLines) == 1:
+            #print("indent subsequent lines")
+            indentedLines = []
+            indentedLines.append(textLines[0])
+            for line in textLines[1:]:
+                indentedLines.append(' '*charPos + line)
+
+            FormattedText = '\n'.join(indentedLines)
+        else:
+            FormattedText = ' '*charPos + FormattedText
+
+    return (FormattedText)
+
+def re_split_line(line):
+    '''
+    take a line string anr return a 3 element list:
+    [ heading spaces (id any), '# '(if any), rest ot the string ]
+    '''
+    r = re.search(r'^(\s*)(#*\s?)(.*)', line)
+    return ( [r.group(1), r.group(2), r.group(3)] )
+
+def get_text(context):
+    #get current text
+    text = getattr(bpy.context.space_data, "text", None)
+
+    #context override for the ops.text.insert() function
+    override = {'window': context.window,
+                'area'  : context.area,
+                'region': context.region,
+                'space': context.space_data,
+                'edit_text' : text
+                }
+    return(text, override)
+
+
+###---TASKS
+
+class debugPrintVariable(bpy.types.Operator):
+    bl_idname = "devtools.debug_print_variable"
+    bl_label = "Debug Print Variable"
+    bl_description = "add a new line with debug print of selected text\n(replace clipboard)"
+    bl_options = {"REGISTER"}
+
+
+    def execute(self, context):
+        #get current text object
+        text, override = get_text(context)
+
+        #create debug print from variable selection
+        charPos = text.current_character
+        clip = copySelected()
+        debugPrint = print_string_variable(clip, linum=text.current_line_index+1)
+        #send charpos at curent indentation (number of whitespace)
+        heading_spaces = re.search('^(\s*).*', text.current_line.body).group(1)
+
+        new = Fixindentation(debugPrint, len(heading_spaces))#to insert at selection level : charPos
+
+        #got to end of line,
+        ### > current_character" from "Text" is read-only
+        #text.current_character = len(text.lines[text.current_line_index].body)
+        bpy.ops.text.move(override, type='LINE_END')
+
+        #put a return and paste with indentation
+        bpy.ops.text.insert(override, text= '\n'+new)
+        return {"FINISHED"}
+
+
+class disableAllDebugPrint(bpy.types.Operator):
+    bl_idname = "devtools.disable_all_debug_print"
+    bl_label = "Disable all debug print"
+    bl_description = "comment all lines finishing with '#Dbg'"
+    bl_options = {"REGISTER"}
+
+    def execute(self, context):
+        text, override = get_text(context)
+        count = 0
+        for i, lineOb in enumerate(text.lines):
+            if lineOb.body.endswith('#Dbg'):#detect debug lines
+                line = lineOb.body
+                splitline = re_split_line(line)
+                if not splitline[1]: #detect comment
+                    count += 1
+                    lineOb.body = splitline[0] + '# ' +  splitline[2]
+                    print ('line {} commented'.format(i))
+        if count:
+            mess = str(count) + ' lines commented'
+        else:
+            mess = 'No line commented'
+        self.report({'INFO'}, mess)
+        return {"FINISHED"}
+
+
+class enableAllDebugPrint(bpy.types.Operator):
+    bl_idname = "my_operator.enable_all_debug_print"
+    bl_label = "Enable all debug print"
+    bl_description = "uncomment all lines finishing wih '#Dbg'"
+    bl_options = {"REGISTER"}
+
+
+    def execute(self, context):
+        text, override = get_text(context)
+        count = 0
+        for i, lineOb in enumerate(text.lines):
+            if lineOb.body.endswith('#Dbg'):#detect debug lines
+                line = lineOb.body
+                splitline = re_split_line(line)
+                if splitline[1]: #detect comment
+                    count += 1
+                    lineOb.body = splitline[0] + splitline[2]
+                    print ('line {} uncommented'.format(i))
+        if count:
+            mess = str(count) + ' lines uncommented'
+        else:
+            mess = 'No line uncommented'
+        self.report({'INFO'}, mess)
+        return {"FINISHED"}
+
+
+
+###---PANEL
+
+class DevTools(bpy.types.Panel):
+    bl_idname = "dev_tools"
+    bl_space_type = "TEXT_EDITOR"
+    bl_region_type = "UI"
+    bl_label = "Dev Tools"
+
+    def draw(self, context):
+        layout = self.layout
+        layout.operator(debugPrintVariable.bl_idname)
+        layout.separator()
+        layout.operator(disableAllDebugPrint.bl_idname)
+        layout.operator(enableAllDebugPrint.bl_idname)
+
+
+
+def register():
+    bpy.utils.register_module(__name__)
+
+def unregister():
+    bpy.utils.unregister_module(__name__)
+
+if __name__ == "__main__":
+    register()
