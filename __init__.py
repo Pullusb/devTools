1dec1f26c85a2de79051bd4288499a7470367d22
pullusb <bernou.samuel@gmail.com>
2024-05-27 16:56:10 +0200

    fix text editor context override
    2.7.3
    
    - fixed: add region in text editor context override (needed in some case)

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "dev tools",
     "description": "Add tools in text editor and console to help development",
     "author": "Samuel Bernou",
-    "version": (2, 7, 2),
+    "version": (2, 7, 3),
     "blender": (3, 0, 0),
     "location": "Text editor > toolbar and console header",
     "doc_url": "https://github.com/Pullusb/devTools",
@@ -815,9 +815,9 @@ class DEV_OT_create_context_override(bpy.types.Operator):
         screen = window.screen
         for area in screen.areas:
             if area.type == '{a.type}':
-                #for region in area.regions:
-                #    if region.type == 'WINDOW':
-                return {{'window': window, 'screen': screen, 'area': area}}#, 'region': region
+                for region in area.regions:
+                    if region.type == 'WINDOW':
+                        return {{'window': window, 'screen': screen, 'area': area, 'region': region}}
 
 with bpy.context.temp_override(**get_override()):
     # contextual ops here
